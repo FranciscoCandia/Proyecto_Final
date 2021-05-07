aede1c3ac49f0a97c194241c9afa465372934b24
francisco antonio <francisco antonio@192.168.1.10>
2021-05-07 07:41:58 -0500

    Proyecto Final

--- src/com/senati/eti/guia1/Caso16.java
+++ src/com/senati/eti/guia1/Caso16.java
@@ -1,10 +1,31 @@
 package com.senati.eti.guia1;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Caso16 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		DecimalFormat df = new DecimalFormat("#.00");
+		
+		System.out.print("Ingrese horas trabajadas........: ");
+		float ht = sc.nextFloat();
+		System.out.print("Ingrese tarifa por hora.........: ");
+		float th = sc.nextFloat();
+		
+		float valor = ht * th;
+		float bono = (float) (valor * 0.05) ;
+		float total = valor + bono;
+		float dolar = (float) (total / 3.24);
+		
+		System.out.println("------------");
+		System.out.println("\nResultados");
+		System.out.println("------------");
+		System.out.println("Sueldo............: " + df.format(valor));
+		System.out.println("Bono..............: " + df.format(bono));
+		System.out.println("Total.............: " + df.format(total));
+		System.out.println("Total en dólares..: " + df.format(dolar));
 
 	}
-
 }
